e1299707066212916139d03588f7880a20e6a99d
gouravlinux <2005gouravsoni@gmail.com>
2026-06-23 10:40:27 +0530

    Problem: best time to buy and sell stocks IV

--- leetcodeProblems/Mar6BestTimeToBuyAndSellStocksIV.java
+++ leetcodeProblems/Mar6BestTimeToBuyAndSellStocksIV.java
@@ -1,26 +1,110 @@
 class Solution {
     int n;
-    int[] curr;
-    int[] ahead;
+    Integer[][] dp;
+
+    private int solve(int i, int[] prices, int txnNo, int k) {
+        if (txnNo == 2 * k || i == n) {
+            // not a valid txn
+            return 0;
+        }
+
+        if (dp[i][txnNo] != null)
+            return dp[i][txnNo];
+
+        int ans = 0;
+
+        if (txnNo % 2 == 0) {
+            // can only buy
+            ans = Math.max(-prices[i] + solve(i + 1, prices, txnNo + 1, k),
+                    0 + solve(i + 1, prices, txnNo, k));
+        } else {
+            // can only sell
+            ans = Math.max(prices[i] + solve(i + 1, prices, txnNo + 1, k),
+                    0 + solve(i + 1, prices, txnNo, k));
+        }
+        return dp[i][txnNo] = ans;
+    }
 
     public int maxProfit(int k, int[] prices) {
-        // (using tabulations)->txns are noed(only 2k can be done)->space optimized
+        /*
+            using recursion + memoization
+            TC : O(2*n*k)
+            SC : O(n*2*k) + O(n) auxilary stack space
+        */
+
         n = prices.length;
-        ahead = new int[2 * k + 1];
-        curr = new int[2 * k + 1];
-        for (int idx = n - 1; idx >= 0; idx--) {
-            for (int txnNo = 2 * k - 1; txnNo >= 0; txnNo--) {
-                //txnNo = 2k has 0 (therefore take from 2k-1)
-                if (txnNo % 2 == 0) {
-                    // can buy
-                    curr[txnNo] = Math.max(-prices[idx] + ahead[txnNo + 1], ahead[txnNo]);
+        dp = new Integer[n][2*k];
+        return solve(0, prices, 0, k);
+    }
+}
+
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        /*
+            using tabulation
+            TC : O(n*2*k)
+            SC : O(n*2*k)
+        */
+
+        int n = prices.length;
+
+        int[][] dp = new int[n + 1][2 * k + 1];
+
+        for (int i = n; i >= 0; i--) {
+            for (int txnNo = 2 * k; txnNo >= 0; txnNo--) {
+                int ans = 0;
+                if (txnNo == 2 * k || i == n) {
+                    ans = 0;
+                } else if (txnNo % 2 == 0) {
+                    // can only buy
+                    ans = Math.max(-prices[i] + dp[i+1][txnNo+1],
+                            0 + dp[i+1][txnNo]);
                 } else {
-                    // can't buy
-                    curr[txnNo] = Math.max(prices[idx] + ahead[txnNo + 1], ahead[txnNo]);
+                    // can only sell
+                    ans = Math.max(prices[i] + dp[i+1][txnNo+1],
+                            0 + dp[i+1][txnNo]);
                 }
+                dp[i][txnNo] = ans;
             }
-            ahead = curr;
         }
-        return curr[0];
+        return dp[0][0];
+    }
+}
+
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        /*
+            using tabulation + space-optimization
+            TC : O(n*2*k)
+            SC : O(2*k)
+        */
+
+        int n = prices.length;
+
+        int[] next = new int[2 * k + 1];
+
+        for (int i = n; i >= 0; i--) {
+            int[] curr = new int[2 * k + 1];
+            for (int txnNo = 2 * k; txnNo >= 0; txnNo--) {
+                int ans = 0;
+                if (txnNo == 2 * k || i == n) {
+                    ans = 0;
+                } else if (txnNo % 2 == 0) {
+                    // can only buy
+                    ans = Math.max(-prices[i] + next[txnNo+1],
+                            0 + next[txnNo]);
+                } else {
+                    // can only sell
+                    ans = Math.max(prices[i] + next[txnNo+1],
+                            0 + next[txnNo]);
+                }
+                curr[txnNo] = ans;
+            }
+            next = curr;
+        }
+        
+        return next[0];
     }
 }
