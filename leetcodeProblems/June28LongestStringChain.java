78b79bf7f2138f388b5fa4e754f962348a193a4e
Gourav Soni <2005gouravsoni@gmail.com>
2026-06-28 11:56:06 +0530

    Problem: Longest String Chain

--- leetcodeProblems/June28LongestStringChain.java
+++ leetcodeProblems/June28LongestStringChain.java
@@ -1,6 +1,52 @@
 import java.util.*;
 
 class Solution {
+    int wordsLen;
+    Integer[][] dp;
+    private boolean isPredecessor(String s1, String s2){
+        int i = 0;
+        int j = 0;
+        int m = s1.length();
+        int n = s2.length();
+        if(n != m+1) return false;
+        while(j < n){
+            if(i < m && s1.charAt(i) == s2.charAt(j)){
+                i++;j++;
+            }else{
+                j++;
+            }
+        }
+        return (i == m && j == n);
+    }
+    private int solve(int i,int prev,String[] words,int wordsLen){
+        if(i == wordsLen){
+            return 0;
+        }
+        int taken = 0, not_taken = 0;
+        if(dp[i][prev+1] != null)
+            return dp[i][prev+1];
+        if(prev == -1 || isPredecessor(words[prev], words[i])){
+            taken = 1 + solve(i+1, i, words, wordsLen);
+        }
+        not_taken = 0 + solve(i+1, prev, words, wordsLen);
+        return dp[i][prev+1] = Math.max(taken, not_taken);
+    }
+    public int longestStrChain(String[] words) {
+        // using memoization
+        // TC : O(wlogw.l) + O(n^2 . l)
+        // SC : O(w^2 + w.l)
+        // where l is max. length of a string word 
+        wordsLen = words.length;
+        Arrays.sort(words, (a,b)->{
+            return Integer.compare(a.length(), b.length());
+        });
+        dp = new Integer[wordsLen][wordsLen];
+        return solve(0, -1, words, wordsLen);
+    }
+}
+
+
+class Solution {
     private boolean checkPossible(String s1, String s2) {
         int m = s1.length();
         int n = s2.length();
@@ -24,6 +70,9 @@ class Solution {
 
     public int longestStrChain(String[] words) {
         // sorting based on length
+        // using tabulation
+        // TC : O(n^2 . l)
+        // SC : O(n^2 + n.l)
         Arrays.sort(words, (a, b) -> {
             return Integer.compare(a.length(), b.length());
         });
