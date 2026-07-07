84372e8be88feaf495114c175d1f0f58b8728b17
Gourav Soni <2005gouravsoni@gmail.com>
2026-07-07 08:53:41 +0530

    Problem: Floyd Warshall Algo

---
+++ gfgProblems/July7FloydWarshallAlgo.java
@@ -0,0 +1,16 @@
+class Solution {
+	public void floydWarshall(int[][] dist) {
+	    // TC : O(n^3)
+	    // SC : O(1)
+		int n = dist.length;
+		int INF = (int)1e8;
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (dist[i][k] != INF && dist[k][j] != INF)
+						dist[i][j] = Math.min(dist[i][j], dist[i][k]+dist[k][j]);
+				}
+			}
+		}
+	}
+}
