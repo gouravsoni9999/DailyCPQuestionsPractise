5f79c631da0a9188955397a7c804fd4d91bd27bd
Gourav Soni <2005gouravsoni@gmail.com>
2026-07-07 07:56:56 +0530

    Problem: Prim's Algo completed

--- gfgProblems/July6PrimsAlgorithm.java
+++ gfgProblems/July6PrimsAlgorithm.java
@@ -17,6 +17,8 @@ class Solution {
         return index;
     }
     public int spanningTree(int V, int[][] edges) {
+        // TC : O(V*V + V*E)
+        // SC : O(V)
         parent = new int[V];
         key = new int[V];
         mstSet = new boolean[V]; // visited nodes array
@@ -64,4 +66,66 @@ class Solution {
         // parent array stores the whole MST, starting with idx having -1
         return minCost;
     }
+}
+
+class Solution {
+	public int spanningTree(int V, int[][] edges) {
+	    // E means no. of edges in edges[][]
+	    // TC : O(V+E) + O(ElogE)
+	    // SC : O(V+E) + O(V) + O(E)
+		// build the adjacency list -> List[Neighbour, weight]
+		List<List<int[]>> adjList = new ArrayList<>();
+		
+		for (int v = 0; v < V; v++) {
+			adjList.add(new ArrayList<>());
+		}
+		
+		for(int[] e : edges){
+		    
+		    int u = e[0];
+		    int v = e[1];
+		    int wt = e[2];
+		    
+		    adjList.get(u).add(new int[]{v,wt});
+		    adjList.get(v).add(new int[]{u,wt});
+		}
+		
+		// using Prim's Algo
+		// but using PriorityQueue
+		
+		// adding in pq -> {node, weight}
+		PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b)-> {
+			return Integer.compare(a[1], b[1]);
+		});
+		
+		boolean[] visited = new boolean[V];
+		
+		int minCost = 0;
+		minHeap.add(new int[] {0, 0});
+		
+		while (!minHeap.isEmpty()) {
+			int[] p = minHeap.poll();
+			
+			int u = p[0];
+			int wt = p[1];
+			
+			if (visited[u])
+				continue;
+			
+			minCost += wt;
+			
+			visited[u] = true; // mark it visited
+			
+			for(int[] e : adjList.get(u)){
+			    
+			    int v = e[0];
+			    
+			    if(!visited[v]){
+			        minHeap.add(e);
+			    }
+			}
+		}
+		
+		return minCost;
+	}
 }
