fee043c1d3c72fa0aa1a9cb1073ca3a131926651
Gourav Soni <2005gouravsoni@gmail.com>
2026-07-10 08:24:49 +0530

    Problem: Maximal Rectangle

--- leetcodeProblems/Mar19MaximalRectangle.java
+++ leetcodeProblems/Mar19MaximalRectangle.java
@@ -1,3 +1,80 @@
+import java.util.*;
+class Solution {
+    int m;
+    int n;
+
+    private int[] findNSE(int[] arr) {
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop(); // greater or equal element in stack not allowed
+            }
+            // if not found, ans is n
+            res[i] = (stack.isEmpty()) ? n : stack.peek();
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+
+    private int[] findPSE(int[] arr) {
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop(); // no greater or equal elements allowed
+            }
+
+            // if not found, ans is -1
+            res[i] = (stack.isEmpty()) ? -1 : stack.peek();
+
+            stack.push(i);
+        }
+
+        return res;
+    }
+
+    private int maxAreaInHistogram(int[] heights) {
+        int[] nse = findNSE(heights);
+        int[] pse = findPSE(heights);
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            int area = (nse[i] - pse[i] - 1) * heights[i];
+            maxArea = Math.max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+
+    public int maximalRectangle(char[][] matrix) {
+        // TC : O(m*(n+n)) ~ O(m*n)
+        // SC : O(n)
+        // using histogram solution
+        m = matrix.length;
+        n = matrix[0].length;
+        int[] heights = new int[n];
+        int maxArea = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // as histogram's base is not there, then whole height becomes 0
+                heights[j] = (matrix[i][j] == '0') ? 0 : heights[j] + 1;
+            }
+
+            // made the histogram
+            int area = maxAreaInHistogram(heights);
+            maxArea = Math.max(area, maxArea);
+        }
+
+        return maxArea;
+    }
+}
+
 class Solution {
     int m;
     int n;
@@ -11,7 +88,7 @@ class Solution {
                 int currIdx = stack.pop();
                 int pse = stack.isEmpty() ? -1 : stack.peek();
                 int nse = i;
-                maxArea = Math.max(maxArea, height[currIdx] * (nse - pse - 1));//height*width
+                maxArea = Math.max(maxArea, height[currIdx] * (nse - pse - 1));// height*width
             }
             stack.push(i);
         }
@@ -19,7 +96,7 @@ class Solution {
             int currIdx = stack.pop();
             int nse = n;
             int pse = stack.isEmpty() ? -1 : stack.peek();
-            maxArea = Math.max(maxArea, height[currIdx] * (nse - pse - 1));//height*width
+            maxArea = Math.max(maxArea, height[currIdx] * (nse - pse - 1));// height*width
         }
         return maxArea;
     }
