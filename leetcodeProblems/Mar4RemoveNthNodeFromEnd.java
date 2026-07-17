222dbad78484082f2a9b89b3ffec7dd0efef45b4
Gourav Soni <2005gouravsoni@gmail.com>
2026-07-17 19:17:43 +0530

    Problem: remove nth node from end in Linked List

--- leetcodeProblems/Mar4RemoveNthNodeFromEnd.java
+++ leetcodeProblems/Mar4RemoveNthNodeFromEnd.java
@@ -1,4 +1,91 @@
 class Solution {
+    private ListNode reverse(ListNode head){
+        if(head == null || head.next == null)
+            return head;
+        
+        ListNode newHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
+    }
+
+    private ListNode removeFirstNode(ListNode newHead){
+        return newHead.next;
+    }
+
+    private ListNode removeNode(ListNode newHead,int n){
+        if(n == 1){
+            return removeFirstNode(newHead);
+        }
+        // remove from start
+        ListNode curr = newHead;
+        ListNode prev = newHead;
+        while(--n > 0){
+            prev = curr;
+            curr = curr.next;
+        }
+        prev.next = curr.next;
+        return newHead;
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // TC : O(n) here n means no. of nodes
+        // SC : O(1) + O(n) recursion stack space
+        // reverse the linked list
+        ListNode newHead = reverse(head);
+        // remove nth node from starting
+        newHead = removeNode(newHead, n);
+        // return reversed linked list
+        return reverse(newHead);
+    }
+}
+
+class Solution {
+    private ListNode reverse(ListNode head){
+        if(head == null || head.next == null)
+            return head;
+        
+        ListNode prev = null;
+        ListNode next;
+        for(ListNode curr = head;curr != null;curr = next){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+
+    private ListNode removeFirstNode(ListNode newHead){
+        return newHead.next;
+    }
+
+    private ListNode removeNode(ListNode newHead,int n){
+        if(n == 1){
+            return removeFirstNode(newHead);
+        }
+        // remove from start
+        ListNode curr = newHead;
+        ListNode prev = newHead;
+        while(--n > 0){
+            prev = curr;
+            curr = curr.next;
+        }
+        prev.next = curr.next;
+        return newHead;
+    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // TC : O(n) here n means no. of nodes
+        // SC : O(1)
+        // reverse the linked list
+        ListNode newHead = reverse(head);
+        // remove nth node from starting
+        newHead = removeNode(newHead, n);
+        // return reversed linked list
+        return reverse(newHead);
+    }
+}
+
+class Solution {
     private int findSize(ListNode head){
         int size = 0;
         while(head != null){
