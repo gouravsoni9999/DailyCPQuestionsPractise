f2daacaa22f4ef3d46a3726e688fb55f460fb28a
Gourav Soni <2005gouravsoni@gmail.com>
2026-09-15 13:25:31 +0530

    Dijkstra's Algo using TreeSet

--- gfgProblems/Sept13DijkstraAlgo.java
+++ gfgProblems/Sept13DijkstraAlgo.java
@@ -10,7 +10,7 @@ class Pair {
     }
 }
 
-class Solution {
+class PriorityQueueSolution {
     public ArrayList<Integer> dijkstra(int V, int[][] edges, int src) {
         // TC : O(ElogV)
         // SC : O(E + V)
@@ -67,4 +67,76 @@ class Solution {
 
         return dist;
     }
+}
+
+class TreeSetSolution {
+	int INF = (int) 1e9;
+	public ArrayList<Integer> dijkstra(int V, int[][] edges, int src) {
+		// TC : O(ElogV)
+		// SC : O(V + E)
+		
+		// make adjList
+		List<List<int[]>> adjList = new ArrayList<>();
+		for (int i = 0; i < V; i++) {
+			adjList.add(new ArrayList<>());
+		}
+		
+		// adding edges in adjList
+		for (int[] edge : edges) {
+			int u = edge[0];
+			int v = edge[1];
+			int wt = edge[2];
+			
+			// since it is an undirected graph
+			adjList.get(u).add(new int[] {v, wt});
+			adjList.get(v).add(new int[] {u, wt});
+		}
+		
+		ArrayList<Integer> dist = new ArrayList<>(); // stores distance from src
+		for (int i = 0; i < V; i++) {
+			dist.add(INF);
+		}
+		
+		// using TreeSet to store {node, dist} in ascending order of dist
+		// if dist is same, but nodes are different then also we write to sort through nodes, otherwise TreeSet would mark it as duplicate
+		TreeSet<int[]> set = new TreeSet<>((a, b) -> {
+			if (a[1] != b[1]) {
+				return Integer.compare(a[1], b[1]);
+			} else {
+				// if distances are same, sort according to node ID
+				return Integer.compare(a[0], b[0]);
+			}
+		});
+		
+		// src is 0 distance away from itself
+		set.add(new int[] {src, 0});
+		dist.set(src, 0);
+		
+		while (!set.isEmpty()) {
+			// take out the smallest distance node
+			int[] it = set.first();
+			int node = it[0];
+			int dis = it[1];
+			set.remove(it); // removes the smallest one
+			
+			for (int[] neigh : adjList.get(node)) {
+				int adjNode = neigh[0];
+				int edgeW = neigh[1];
+				
+				if (dis + edgeW < dist.get(adjNode)) {
+					// erase if it already existed in set
+					if (dist.get(adjNode) != INF) {
+						// some other node reached it already
+						set.remove(new int[] {adjNode, dist.get(adjNode)});
+					}
+					// update the dist through node
+					dist.set(adjNode, dis + edgeW);
+					// push into the set
+					set.add(new int[] {adjNode, dis + edgeW});
+				}
+			}
+		}
+		
+		return dist;
+	}
 }
